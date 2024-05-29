56ae997c90445f1078ebfc86d5709c511dbee4aa
Matei-Alexandru Gardus <matei@gard.us>
2024-05-28 19:32:18 -0700

    Convert atop profile files into JSONL

--- redis-benchmark/ecs/stop-and-upload.sh
+++ redis-benchmark/ecs/stop-and-upload.sh
@@ -10,29 +10,29 @@ fi
 
 SERVICE=$1
 DATE=$(date +'%Y%m%d%H%M')
-FILE="/var/log/redis_profile_${SERVICE}_${DATE}.log"
+FILE="/var/log/redis_profile_${SERVICE}_${DATE}.jsonl"
 
 echo "Stopping atop process..."
-if pgrep atop > /dev/null; then
-    pkill atop
-    echo "atop process stopped."
+if pgrep atop >/dev/null; then
+	pkill atop
+	echo "atop process stopped."
 else
-    echo "No atop process found."
-    exit 1
+	echo "No atop process found."
+	exit 1
 fi
 
 if [ -f /var/log/atop.log ]; then
-    mv /var/log/atop.log "$FILE"
-    echo "Renamed atop log file to $FILE."
+	atop -r /var/log/atop.log -J ALL >"$FILE"
+	echo "Converted atop profile to JSON in $FILE"
 else
-    echo "No atop log file found."
-    exit 1
+	echo "No atop log file found."
+	exit 1
 fi
 
 echo "Uploading log file to S3..."
 if /usr/local/bin/upload_to_s3 "$FILE"; then
-    echo "Log file uploaded to S3."
+	echo "Log file uploaded to S3."
 else
-    echo "/usr/local/bin/upload_to_s3 script not found or not executable."
-    exit 1
+	echo "/usr/local/bin/upload_to_s3 script not found or not executable."
+	exit 1
 fi
